43d1965e2010210cf328bde7430c24e2148185d4
Rita Doumet <rita.17.2001@gmail.com>
2022-04-14 21:54:34 +0300

    Added sound effect
    Added rolling dice sound effect when the dice is clicked.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const adviceID = document.getElementById("adviceID");
 const advice = document.getElementById("advice");
+const soundEffect = new Audio('media/dice_roll_sound.mp3');
 function rollDice(){
     fetch("https://api.adviceslip.com/advice")
     .then(res => res.json())
@@ -7,5 +8,6 @@ function rollDice(){
         adviceID.innerHTML=data.slip.id;
         advice.innerHTML="“"+data.slip.advice+"”";
     })
+    soundEffect.play();
 }
 rollDice();
